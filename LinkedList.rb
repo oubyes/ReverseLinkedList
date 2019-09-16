25925beacaaa825d6a4b1d8532e66f5bf6847deb
Seybou <seybou@yahoo.com>
2019-09-16 04:38:46 +0000

    Initial commit

---
+++ LinkedList.rb
@@ -0,0 +1,66 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+
+class Stack
+  attr_reader :data
+
+  def initialize
+    @data = nil
+  end
+
+  def push(value)
+    @data = LinkedListNode.new(value, @data)
+  end
+
+  def pop
+    return print "nil\n" if @data.nil?
+    print "#{@data.value}\n"
+    @data = @data.next_node
+  end
+end
+
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def reverse_list(list, previous=nil)
+  current_head = list.next_node
+  list.next_node = previous
+  if current_head
+    reverse_list(current_head, list)
+  else
+    list
+  end
+end
+
+def reverse_list(list)
+  stack = Stack.new
+  while list
+    stack.push(list.value)
+    list = list.next_node
+  end
+
+  return stack.data
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+print_values(node3)
+puts "------"
+print_values(reverse_list(node3))
+
